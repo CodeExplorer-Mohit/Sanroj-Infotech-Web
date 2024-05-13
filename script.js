19a3b7b5d6aae963255198fa31c9b31caa279cb7
CodeExplorer-Mohit <jatavmohit747.com>
2024-05-13 14:46:59 +0530

    updated file

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.addEventListener("scroll", () => {
+//   let reveals = document.querySelector(".reveal");
+//   console.log(reveals);
+//   for (let i = 0; i < reveals.clientHeight; i++) {
+//     let windowHeight = window.innerHeight;
+//     console.log(windowHeight);
+//     let revealTop = reveals[i].getBoundingClientRect().top;
+//     console.log(revealTop);
+//   }
+// });
