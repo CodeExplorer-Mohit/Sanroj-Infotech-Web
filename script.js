cdaa316bb019490e1a8c0e4de61588f60f602824
CodeExplorer-Mohit <jatavmohit747.com>
2024-05-13 18:25:14 +0530

    updated files

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const serviceAnimText = document.getElementById("service-anim-text");
 document.addEventListener("click", (e) => {
   let defaultValue = serviceAnimText.innerHTML;
   let serviceValue = e.target.getAttribute("value");
-  serviceAnimText.innerHTML = serviceValue ? serviceValue : defaultValue;
+  serviceAnimText.classList.add("scale-animation");
+  setTimeout(() => {
+    serviceAnimText.innerHTML = serviceValue ? serviceValue : defaultValue;
+    serviceAnimText.classList.remove("scale-animation");
+  }, 300); // Adjust according to transition duration
 });
