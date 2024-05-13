1377c9f3e9731bbf7dd9655cfd5373b19d066f6f
CodeExplorer-Mohit <jatavmohit747.com>
2024-05-13 18:07:14 +0530

    updated files

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// document.addEventListener("scroll", () => {
-//   let reveals = document.querySelector(".reveal");
-//   console.log(reveals);
-//   for (let i = 0; i < reveals.clientHeight; i++) {
-//     let windowHeight = window.innerHeight;
-//     console.log(windowHeight);
-//     let revealTop = reveals[i].getBoundingClientRect().top;
-//     console.log(revealTop);
-//   }
-// });
+const serviceAnimText = document.getElementById("service-anim-text");
+
+document.addEventListener("click", (e) => {
+  // console.log(e.target.getAttribute("value"));
+  let defaultValue = serviceAnimText.innerHTML;
+  let serviceValue = e.target.getAttribute("value");
+  serviceAnimText.innerHTML = serviceValue ? serviceValue : defaultValue;
+});
