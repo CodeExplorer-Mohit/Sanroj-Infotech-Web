b9c471e2d8fa24a859331ab4a534a4c9c95f7425
CodeExplorer-Mohit <jatavmohit747.com>
2024-05-13 18:14:48 +0530

    updated files

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const serviceAnimText = document.getElementById("service-anim-text");
 
 document.addEventListener("click", (e) => {
-  // console.log(e.target.getAttribute("value"));
   let defaultValue = serviceAnimText.innerHTML;
   let serviceValue = e.target.getAttribute("value");
   serviceAnimText.innerHTML = serviceValue ? serviceValue : defaultValue;
