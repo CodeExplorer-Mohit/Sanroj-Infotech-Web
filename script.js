7f0785b321e2c2b8e0e637ea8466156865545212
CodeExplorer-Mohit <jatavmohit747.com>
2024-05-15 00:27:12 +0530

    updated files

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
+// const serviceAnimText = document.getElementById("service-anim-text");
+
+// const servicesTextData = [
+//   "Ebooks Services",
+//   "Cloud Service",
+//   "Database Management",
+//   "Outsourcing",
+//   "Domain Hosting",
+//   "Digital Marketing",
+//   "Ecommerce Website",
+//   "Software Development",
+// ];
+// let i = 0;
+// setInterval(() => {
+//   serviceAnimText.innerHTML = servicesTextData[(i === 7 ? (i = 0) : i++) % 7];
+//   serviceAnimText.classList.add("scale-animation");
+//   setTimeout(() => {
+//     serviceAnimText.classList.remove("scale-animation");
+//   }, 300);
+// }, 2000);
+
 const serviceAnimText = document.getElementById("service-anim-text");
-
-// window.addEventListener("scroll", () => {
-//   let reveals = document.querySelectorAll(".reveal");
-
-//   for (let i of reveals) {
-//     let windowHeight = window.innerHeight;
-//     let revealTop = i.getBoundingClientRect().top;
-//     let revealPoint = 150;
-
-//     if (revealTop < windowHeight - revealPoint) {
-//       i.classList.add("active");
-//     } else {
-//       i.classList.remove("active");
-//     }
-//     // console.log(revealTop);
-//   }
-// });
 
 const servicesTextData = [
   "Ebooks Services",
@@ -27,21 +31,13 @@ const servicesTextData = [
   "Ecommerce Website",
   "Software Development",
 ];
-
-// document.addEventListener("click", (e) => {
-//   let defaultValue = serviceAnimText.innerHTML;
-//   let serviceValue = e.target.getAttribute("value");
-//   serviceAnimText.classList.add("scale-animation");
-//   setTimeout(() => {
-//     serviceAnimText.innerHTML = serviceValue ? serviceValue : defaultValue;
-//     serviceAnimText.classList.remove("scale-animation");
-//   }, 300);
-// });
 let i = 0;
+
 setInterval(() => {
-  serviceAnimText.innerHTML = servicesTextData[(i === 7 ? (i = 0) : i++) % 7];
+  serviceAnimText.innerHTML = servicesTextData[i];
   serviceAnimText.classList.add("scale-animation");
   setTimeout(() => {
     serviceAnimText.classList.remove("scale-animation");
   }, 300);
+  i = (i + 1) % servicesTextData.length;
 }, 2000);
