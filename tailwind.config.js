5533dd1fe8d3694cf4709287b601dd4f1e36b88e
CodeExplorer-Mohit <jatavmohit747.com>
2024-05-15 20:35:39 +0530

    updated files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./pages/service/*.{html,js}", "./index.html/*.{html,js}"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
